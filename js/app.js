538847db0279b2f46451e7c70bc65aa8ec4d3cff
capmahin <mahinulhuque1996@gmail.com>
2022-02-13 09:00:28 +0600

    and id key-pad and id typed-numbers and creat 3condition

--- js/app.js
+++ js/app.js
@@ -14,3 +14,18 @@ function generatePin() {
   const pin = getPin();
   document.getElementById("display-pin").value = pin;
 }
+
+// pin-matcher key-pad
+document.getElementById("key-pad").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const calcInput = document.getElementById("typed-numbers");
+  if (isNaN(number)) {
+    if (number == "C") {
+      calcInput.value = "";
+    }
+  } else {
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+  }
+});
